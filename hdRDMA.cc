ddb1a6c24f9ee7ee5f2e5fe5852b85e087ee23b8
Lucas Zadrozny <lucas.zadrozny@hotmail.com>
2022-11-19 02:19:21 +1100

    Fixed pause on disconnect

--- hdRDMA.cc
+++ hdRDMA.cc
@@ -3,6 +3,10 @@
 
 #ifdef __GNUC__
 #include <unistd.h>
+#include <sys/mman.h> // mmap, munmap
+#include <linux/mman.h>
+#include <pthread.h>
+#include <csignal>
 #endif
 
 #include <string.h>
@@ -19,9 +23,6 @@ using namespace std::chrono;
 using std::chrono::duration;
 using std::chrono::duration_cast;
 using std::chrono::high_resolution_clock;
-
-#include <sys/mman.h> // mmap, munmap
-#include <linux/mman.h>
 
 template <typename T>
 struct thp_allocator
@@ -292,6 +293,12 @@ hdRDMA::~hdRDMA()
 	if(           pd!=nullptr ) ibv_dealloc_pd( pd );
 	if(          ctx!=nullptr ) ibv_close_device( ctx );
 
+#ifdef __GNUC__
+	// 'linux_rdma' wakes up their async event file descriptor with a SIGINT. Here: https://github.com/linux-rdma/rdma-core/blob/3b28e0e4784cce3aceedab39e1ae102538e212e2/srp_daemon/srp_daemon.c#L2017
+	// I tried using async file descriptors with polling, but that didn't seem to help. Oh well, this works I guess.
+	pthread_kill(ack_thread->native_handle(), SIGINT);
+#endif
+
 	ack_thread->join();
 	delete ack_thread;
 	ack_thread = nullptr;
@@ -465,7 +472,7 @@ void hdRDMA::GetBuffers( std::vector<hdRDMAThread::bufferinfo> &buffers, int Nre
 {
 	std::unique_lock grd( buffer_pool_mutex );
 
-	while (!done && !buffers.size())
+	while (!done && !buffer_pool.size())
 	{
 		buffer_pool_cond.wait(grd);
 	}
